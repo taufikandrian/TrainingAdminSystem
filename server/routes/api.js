c731977e964c1e67bf49666c6c539eabd7c5c019
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-09-05 16:54:53 +0800

    user login

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,33 +2,33 @@ const express = require('express');
 const router  = express.Router();
 const fs      = require('fs');
 
-var user  = require("../model/user");
-var grade = require("../model/grade");
+// var user  = require("../models/user");
+// var grade = require("../models/grade");
 
 /* GET api listing. */
+// API for TAS
+// router.get('/grades', function(req, res) {
+// 	grade.getRecords(function(status, data){
+// 		if(status > 0) {
+// 			res.writeHead(200, {"Content-Type": "text/json"});
+// 			res.end(JSON.stringify(data));
+// 		}
+// 		else {console.log(data);}
+// 	}, '');
+// });
+
+// router.get('/users', function(req, res) {
+// 	user.getRecords(function(status, data){
+// 		if(status > 0) {
+// 			res.writeHead(200, {"Content-Type": "text/json"});
+// 			res.end(JSON.stringify(data));
+// 		}
+// 		else {console.log(data);}
+// 	}, '');
+// });
+
 router.get('/', (req, res) => {
   res.send('api works');
-});
-
-// API for TAS
-router.get('/grades', function(req, res) {
-	grade.getRecords(function(status, data){
-		if(status > 0) {
-			res.writeHead(200, {"Content-Type": "text/json"});
-			res.end(JSON.stringify(data));
-		}
-		else {console.log(data);}
-	}, '');
-});
-
-router.get('/users', function(req, res) {
-	user.getRecords(function(status, data){
-		if(status > 0) {
-			res.writeHead(200, {"Content-Type": "text/json"});
-			res.end(JSON.stringify(data));
-		}
-		else {console.log(data);}
-	}, '');
 });
 
 router.post('/auth', function(req, res) {
