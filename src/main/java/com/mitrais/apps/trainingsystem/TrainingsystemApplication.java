01f276dc8ef5976ea7400cd61ab0cb9e0554c0ea
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-09-08 17:32:15 +0800

    integrate angular2 and SpringBoot

---
+++ src/main/java/com/mitrais/apps/trainingsystem/TrainingsystemApplication.java
@@ -0,0 +1,12 @@
+package com.mitrais.apps.trainingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingsystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mitrais/apps/trainingsystem/TrainingsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mitrais.apps.trainingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingsystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
