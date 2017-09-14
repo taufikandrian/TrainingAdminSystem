a22ab0c667a729f8ffb1a9316df1d1b991dcd1ed
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-09-14 17:11:07 +0800

    update base URL

---
+++ src/main/java/com/mitrais/apps/trainingsystem/controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.mitrais.apps.trainingsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+public abstract class BaseController {
+
+}
--- src/main/java/com/mitrais/apps/trainingsystem/controller/UserController.java
+++ src/main/java/com/mitrais/apps/trainingsystem/controller/UserController.java
@@ -1,43 +1,22 @@
 package com.mitrais.apps.trainingsystem.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mitrais.apps.trainingsystem.model.User;
 import com.mitrais.apps.trainingsystem.repository.UserRepository;
 
 @RestController
-@RequestMapping("/api/users/")
-public class UserController {
+public class UserController extends BaseController {
 	
 	@Autowired
 	private UserRepository userRepo;
 	
-	@GetMapping(value="all",produces="application/json")
-    @ResponseBody
-    public Map<String, String> show() {
-           ObjectMapper mapper = new ObjectMapper();
-           String jsonInString = null;
-           
-           try {
-                  jsonInString = mapper.writeValueAsString(userRepo.findAll());
-           } catch (JsonProcessingException e) {
-                  e.printStackTrace();
-           }
-           
-           //Create Response
-           Map<String, String> response = new HashMap<>();
-           
-           response.put("status", "true");
-           response.put("message", jsonInString);
-           
-     return response;
-    }
+	@RequestMapping("/users/all")
+    public List<User> getAll() {
+		return (List<User>) this.userRepo.findAll();
+	}
 }
