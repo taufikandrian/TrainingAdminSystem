72fdcd901c06c346e915a204a1b07a76833a4899
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-09-14 15:28:38 +0800

    fixing some issues in index angular

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -38,7 +38,7 @@ export const slideInOutAnimation =
            style({
                // start with the content positioned off the right of the screen,
                // -400% is required instead of -100% because the negative position adds to the width of the element
-               right: '-400%',
+               right: '-100%',
 
                // start with background opacity set to 0 (invisible)
                backgroundColor: 'rgba(0, 0, 0, 0)'
@@ -59,7 +59,7 @@ export const slideInOutAnimation =
            // animation and styles at end of transition
            animate('.5s ease-in-out', style({
                // transition the right position to -400% which slides the content out of view
-               right: '-400%',
+               right: '-100%',
 
                // transition the background opacity to 0 to fade it out
                backgroundColor: 'rgba(0, 0, 0, 0)'
