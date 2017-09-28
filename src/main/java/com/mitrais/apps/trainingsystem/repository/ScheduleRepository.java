195e580ffe2673e50501629747324deb2dc8e98c
taufikandrian <taufikandrian18@gmail.com>
2017-09-28 15:45:10 +0800

    "penambahan schedulle controller"

--- src/main/java/com/mitrais/apps/trainingsystem/repository/ScheduleRepository.java
+++ src/main/java/com/mitrais/apps/trainingsystem/repository/ScheduleRepository.java
@@ -3,9 +3,8 @@ package com.mitrais.apps.trainingsystem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import com.mitrais.apps.trainingsystem.model.Training;
 import com.mitrais.apps.trainingsystem.model.TrainingCourse;
 
-public interface ScheduleRepository extends JpaRepository<TrainingCourse, String>, JpaSpecificationExecutor<Training> {
+public interface ScheduleRepository extends JpaRepository<TrainingCourse, String>, JpaSpecificationExecutor<TrainingCourse> {
 	TrainingCourse findByTrainingId(String trainingId);
 }
