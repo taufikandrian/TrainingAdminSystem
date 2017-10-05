40c55db699364354c76aa318351e3b1ffc424d31
taufikandrian <taufikandrian18@gmail.com>
2017-10-05 14:26:11 +0800

    Repository user course sama user course detail

---
+++ src/main/java/com/mitrais/apps/trainingsystem/repository/UserCourseDetailRepository.java
@@ -0,0 +1,10 @@
+package com.mitrais.apps.trainingsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.mitrais.apps.trainingsystem.model.UserCourseDetail;
+
+public interface UserCourseDetailRepository extends JpaRepository<UserCourseDetail, String>, JpaSpecificationExecutor<UserCourseDetail> {
+
+}
