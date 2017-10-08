9a0332a1777f2b7ddfc9cbcd0dca5152eda9de4f
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-10-09 07:59:05 +0800

    deployment

--- src/app/classes/Environment.ts
+++ src/app/classes/Environment.ts
@@ -1,6 +1,6 @@
 export const Environment = {
   production: false,
   envName: 'dev',
-  apiUrl: 'http://172.19.14.125:8081/api',
+  apiUrl: 'http://localhost:8081/api',
   // apiUrl: 'http://172.19.14.113:8081/api'
 };
